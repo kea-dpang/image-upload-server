9edc3911ea06b3a5ac2d78270a80538b6c9845c4
SeungHyeon <namsh1125@naver.com>
2024-01-28 14:42:59 +0900

    Feat: Feign 클라이언트를 이용한 파일 업로드 기능 구현

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ extra["springCloudVersion"] = "2023.0.0"
 dependencies {
     // Spring
     implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
---
+++ src/main/kotlin/kea/dpang/upload/feign/KakaoCloudObjectStorageClient.kt
@@ -0,0 +1,36 @@
+package kea.dpang.upload.feign
+
+import feign.Response
+import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.multipart.MultipartFile
+
+/**
+ * 파일 업로드 및 삭제를 위한 Feign 클라이언트 인터페이스
+ */
+@FeignClient(name = "KakaoCloudObjectStorage", url = "https://objectstorage.kr-gov-central-1.kakaoicloud-kr-gov.com")
+interface KakaoCloudObjectStorageClient {
+
+    /**
+     * 파일 업로드 메소드
+     *
+     * @param account 계정 ID
+     * @param bucket 버킷 이름
+     * @param path 파일 경로
+     * @param file 파일 이름
+     * @param token 사용자 인증 토큰
+     * @param contentType 콘텐츠 타입
+     * @param data 업로드할 파일 데이터
+     * @return HTTP 응답
+     */
+    @PutMapping("/v1/{account}/{bucket}/{path}/{file}", consumes = ["multipart/form-data"])
+    fun uploadFile(
+        @PathVariable("account") account: String,
+        @PathVariable("bucket") bucket: String,
+        @PathVariable("path") path: String,
+        @PathVariable("file") file: String,
+        @RequestHeader("X-Auth-Token") token: String,
+        @RequestBody data: MultipartFile
+    ): Response
+
+}
